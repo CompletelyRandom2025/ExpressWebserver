2d8c3571e38a53ee7ab450ed2c1a53068221c343
CompletelyRandom2025 <cag.shields23@gmail.com>
2025-09-02 15:03:04 +0100

    fetch javascript

--- assets/contentload.js
+++ assets/contentload.js
@@ -1,10 +1,14 @@
 response2="temp";
-(async () => {
+async function get(id, type)
+{
 	await fetch("https://uselessfacts.jsph.pl/api/v2/facts/random").then(async Response => response2=Response);
     response2 = await response2.json();
     console.log(response2.text);
-    const paragraph = document.createElement('p');
+    const paragraph = document.createElement(type);
     paragraph.textContent = response2.text;
-    const content = document.getElementById('jscontent');
+    const content = document.getElementById(id);
     content.appendChild(paragraph);
-})();
+}
+get('jscontent', 'h3');
+get('jscontent2', 'h3');
+get('jscontent3', 'h3');
